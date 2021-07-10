4eb18dcf62f2f61a5df7ec9dacbe9f596beb23f8
safinghoghabori <safinghoghabori65@gmail.com>
2021-07-11 00:02:55 +0530

    contactcard and viewcontacts file css added and some other files changed

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,34 +1,23 @@
+import "./contactcard.css";
+
 const ContactCard = ({ contact }) => {
   return (
-    // <div className="card">
-    //   <p>
-    //     First Name: <span>{contact.firstname}</span>
-    //   </p>
-    //   <p>
-    //     Last Name: <span>{contact.lastname}</span>
-    //   </p>
-    //   <p>
-    //     Email: <span>{contact.email}</span>
-    //   </p>
-    //   <p>
-    //     Address: <span>{contact.address}</span>
-    //   </p>
-    // </div>
-
-    <div className="card">
-      <p>
-        First Name: <span>SDFASDF</span>
-      </p>
-      <p>
-        Last Name: <span>doe</span>
-      </p>
-      <p>
-        Email: <span>johndoe@gmail.com</span>
-      </p>
-      <p>
-        Address: <span>san fransisco, CA</span>
-      </p>
-    </div>
+    <>
+      <div className="card">
+        <p>
+          First Name: <span>{contact.firstname}</span>
+        </p>
+        <p>
+          Last Name: <span>{contact.lastname}</span>
+        </p>
+        <p>
+          Email: <span>{contact.email}</span>
+        </p>
+        <p>
+          Address: <span>contact.address</span>
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/pages/ViewContacts/ViewContacts.jsx
+++ src/pages/ViewContacts/ViewContacts.jsx
@@ -1,6 +1,10 @@
 import ContactCard from "../../components/ContactCard/ContactCard";
+import { useHistory } from "react-router-dom";
+
+import "./viewcontacts.css";
 
 const ViewContacts = () => {
+  const history = useHistory();
   const data = [
     {
       firstname: "john",
@@ -34,11 +38,29 @@ const ViewContacts = () => {
     },
   ];
 
+  const handleBackBtn = () => {
+    history.push("/");
+  };
+
   return (
-    // <>{data && data.map((contact) => <ContactCard contact={contact} />)}</>
+    // <></>
     <>
-      {" "}
-      <ContactCard />
+      <div className="heading">
+        <h1>View Contacts</h1>
+      </div>
+      <div className="card__main">
+        {data &&
+          data.map((contact, i) => <ContactCard key={i} contact={contact} />)}
+      </div>
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <button
+          className="backbtn btn"
+          style={{ background: "wheat" }}
+          onClick={handleBackBtn}
+        >
+          Back
+        </button>
+      </div>
     </>
   );
 };
