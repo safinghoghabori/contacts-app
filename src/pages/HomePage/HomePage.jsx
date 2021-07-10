2dae7a3006c39df1691ac8b2529e3a5b4d8e3242
safinghoghabori <safinghoghabori65@gmail.com>
2021-07-10 19:08:21 +0530

    homepage css file added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,24 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import "./homepage.css";
 
 const HomePage = () => {
-    return (
-        <div className='container'>
-            <div className='heading'>
-                <h1>Contacts App</h1>
-                <p>#DPSCHALLANGE</p>
-            </div>
-            <div className="link__buttons">
-                <Link to='/add-contact'>Add Contact</Link>
-            </div>
-            <div className="link__buttons">
-                <Link to='/view-contacts'>View Contacts</Link>
-            </div>
+  return (
+    <>
+      <div className="heading">
+        <h1>Contacts App</h1>
+        <p>#DPSCHALLANGE</p>
+      </div>
+      <div className="container">
+        <div className="link__buttons">
+          <Link to="/add-contact">Add Contact</Link>
         </div>
-    )
-}
+        <div className="link__buttons">
+          <Link to="/view-contacts">View Contacts</Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
