c31c0f4f7fed34b657311ad7846238f7f86fdb1d
safinghoghabori <safinghoghabori65@gmail.com>
2021-07-11 12:33:54 +0530

    add contact functionality added successfully

--- src/pages/AddContact/AddContact.jsx
+++ src/pages/AddContact/AddContact.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 import "./addcontact.css";
 
 const AddContact = () => {
   const history = useHistory();
 
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+
+  const [msg, setMsg] = useState(false);
+
   const handleBackBtn = () => {
     history.push("/");
   };
 
-  const handleAddBtn = () => {};
+  const handleSubmit = (e) => e.preventDefault();
+
+  const handleAddBtn = async () => {
+    await axios.post("/api/add-contact", {
+      firstname,
+      lastname,
+      email,
+      address,
+    });
+
+    setMsg(true);
+
+    setTimeout(() => {
+      history.push("/view-contacts");
+    }, 1000);
+  };
 
   return (
     <div className="container">
@@ -17,26 +41,48 @@ const AddContact = () => {
         <div className="form__heading">
           <h1>Add Contact Form</h1>
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="field__area">
             <label htmlFor="firstname">First Name</label>
-            <input type="text" name="firstname" required />
+            <input
+              type="text"
+              name="firstname"
+              onChange={(e) => setFirstname(e.target.value)}
+              required
+            />
           </div>
 
           <div className="field__area">
             <label htmlFor="lastname">Last Name</label>
-            <input type="text" name="lastname" required />
+            <input
+              type="text"
+              name="lastname"
+              onChange={(e) => setLastname(e.target.value)}
+              required
+            />
           </div>
 
           <div className="field__area">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" required />
+            <input
+              type="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
 
           <div className="field__area">
             <label htmlFor="address">Address</label>
-            <textarea name="address" rows="5" required></textarea>
+            <textarea
+              name="address"
+              rows="5"
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            ></textarea>
           </div>
+
+          {msg && <div className="success">Contact added successfully...</div>}
 
           <div className="form__btn">
             <button className="backbtn btn" onClick={handleBackBtn}>
