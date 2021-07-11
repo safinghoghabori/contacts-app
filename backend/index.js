d4cf2bb39f65af0d4715070144265968cd1ecda3
safinghoghabori <safinghoghabori65@gmail.com>
2021-07-11 13:53:55 +0530

    backend index.js file changed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,10 @@ app.use(express.json());
 
 //import model
 const Contact = require("./src/model/contact");
+
+/*[Deployment: to heroku] */
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
